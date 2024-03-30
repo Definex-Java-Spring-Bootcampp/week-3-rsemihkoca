a00914e0dec942a39eb416829b4da104e2b29cdb
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-30 23:38:28 +0300

    part2 finished:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/repository/ApplicationRepository.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/repository/ApplicationRepository.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/repository/ApplicationRepository.java
@@ -1,10 +1,14 @@
 package com.patika.kredinbizdeservice.repository;
 
 import com.patika.kredinbizdeservice.model.Application;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
+@Scope(value = "singleton")
 public class ApplicationRepository {
 
     private List<Application> applicationList = new ArrayList<>();
@@ -12,5 +16,12 @@ public class ApplicationRepository {
     public Application save(Application application) {
         applicationList.add(application);
         return application;
+    }
+
+    public List<Application> getAll(String email) {
+
+        return applicationList.stream()
+                .filter(application -> application.getUser().getEmail().equals(email))
+                .toList();
     }
 }
