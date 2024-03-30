440e62370166cd5f9b93daf6c41195ca37f2870e
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-30 23:38:28 +0300

    part2 finished:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/User.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/User.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/model/User.java
@@ -1,102 +1,47 @@
 package com.patika.kredinbizdeservice.model;
 
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class User {
 
+    private Long id;
     private String name;
     private String surname;
-   // private LocalDate birthDate;
     private String email; //bir email ile bir kere kayıt olunabilir.
     private String password; //hash fonskiyonlarından biri ile hashlanecek.
     private String phoneNumber;
     private Boolean isActive;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<Application> applicationList;
 
     public User() {
+        this.id = 0L;
+        this.name = null;
+        this.surname = null;
+        this.email = null;
+        this.password = null;
+        this.phoneNumber = null;
+        this.isActive = null;
     }
 
-    public User(String name, String surname, LocalDate birthDate, String email, String password, String phoneNumber, Boolean isActive) {
+    public User(Long id,
+                String name, String surname, String email, String password, String phoneNumber, Boolean isActive) {
+        this.id = id;
         this.name = name;
         this.surname = surname;
-      ///  this.birthDate = birthDate;
         this.email = email;
         this.password = password;
         this.phoneNumber = phoneNumber;
         this.isActive = isActive;
     }
 
-    public User(String name, String surname, String email, String password, String phoneNumber, Boolean isActive) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-        this.isActive = isActive;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
-/*
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-*/
-    public List<Application> getApplicationList() {
-        return applicationList;
-    }
-
-    public void setApplicationList(List<Application> applicationList) {
-        this.applicationList = applicationList;
-    }
 }
