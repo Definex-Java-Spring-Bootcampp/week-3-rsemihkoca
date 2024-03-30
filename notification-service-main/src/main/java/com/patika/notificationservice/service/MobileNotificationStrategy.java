431f7cdf95a71f955b6606505dd295eac57a0ff6
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-30 23:38:28 +0300

    part2 finished:notification-service-main/src/main/java/com/patika/notificationservice/service/MobileNotificationStrategy.java

--- notification-service-main/src/main/java/com/patika/notificationservice/service/MobileNotificationStrategy.java
+++ notification-service-main/src/main/java/com/patika/notificationservice/service/MobileNotificationStrategy.java
@@ -1,4 +1,15 @@
 package com.patika.notificationservice.service;
 
-public class MobileNotificationStrategy {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@Scope(value = "prototype")
+public class MobileNotificationStrategy implements NotificationStrategy{
+    @Override
+    public void sendNotification(String message) {
+        System.out.println("Mobile notification sent: " + message);
+    }
 }
