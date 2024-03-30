bc24343d6195176333009d48cbfdae087166a05c
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-30 23:38:28 +0300

    part2 finished:notification-service-main/src/main/java/com/patika/notificationservice/service/EmailNotificationStrategy.java

--- notification-service-main/src/main/java/com/patika/notificationservice/service/EmailNotificationStrategy.java
+++ notification-service-main/src/main/java/com/patika/notificationservice/service/EmailNotificationStrategy.java
@@ -1,4 +1,15 @@
 package com.patika.notificationservice.service;
 
-public class EmailNotificationStrategy {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@Scope(value = "prototype")
+public class EmailNotificationStrategy implements NotificationStrategy{
+    @Override
+    public void sendNotification(String message) {
+        log.info("Email gönderildi: {}", message);
+    }
 }
