290e1b4b1fa407d32a703d9e51ad0f08ea0142cd
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-30 23:38:27 +0300

    part2 finished:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/client/garanti/GarantiServiceClient.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/client/garanti/GarantiServiceClient.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/client/garanti/GarantiServiceClient.java
@@ -1,5 +1,6 @@
-package com.patika.kredinbizdeservice.client;
+package com.patika.kredinbizdeservice.client.garanti;
 
+import com.patika.kredinbizdeservice.client.BankServiceClient;
 import com.patika.kredinbizdeservice.client.dto.request.BankApplicationRequest;
 import com.patika.kredinbizdeservice.client.dto.response.ApplicationResponse;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = "garanti-service", url = "localhost:5555")
-public interface GarantiServiceClient extends BankServiceClient{
+public interface GarantiServiceClient extends BankServiceClient {
 
     @PostMapping("api/garanti/v1/applications")
     ApplicationResponse createApplication(@RequestBody BankApplicationRequest request);
