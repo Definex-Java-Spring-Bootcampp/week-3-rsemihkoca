0818ea720503aaeb533a60f0ead0951bcb1af2e0
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-30 23:38:28 +0300

    part2 finished:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/service/ApplicationService.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/service/ApplicationService.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/service/ApplicationService.java
@@ -1,26 +1,33 @@
 package com.patika.kredinbizdeservice.service;
 
-import com.patika.kredinbizdeservice.client.AkbankServiceClient;
-import com.patika.kredinbizdeservice.client.dto.request.AkbankApplicationRequest;
-import com.patika.kredinbizdeservice.client.dto.response.ApplicationResponse;
+import com.patika.kredinbizdeservice.client.BankClientFactory;
+import com.patika.kredinbizdeservice.client.BankServiceClient;
+import com.patika.kredinbizdeservice.client.dto.request.BankApplicationRequest;
 import com.patika.kredinbizdeservice.converter.ApplicationConverter;
 import com.patika.kredinbizdeservice.dto.request.ApplicationRequest;
+import com.patika.kredinbizdeservice.enums.BankType;
 import com.patika.kredinbizdeservice.model.Application;
 import com.patika.kredinbizdeservice.model.User;
+import com.patika.kredinbizdeservice.producer.NotificationProducer;
+import com.patika.kredinbizdeservice.producer.dto.NotificationDTO;
+import com.patika.kredinbizdeservice.producer.enums.NotificationType;
 import com.patika.kredinbizdeservice.repository.ApplicationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ApplicationService {
 
-    private final ApplicationRepository applicationRepository = new ApplicationRepository();
+    private final ApplicationRepository applicationRepository;
     private final ApplicationConverter applicationConverter;
     private final UserService userService;
-    private final AkbankServiceClient akbankServiceClient;
+    private final BankClientFactory bankClientFactory;
+    private final NotificationProducer notificationProducer;
 
     public Application createApplication(ApplicationRequest request) {
 
@@ -31,16 +38,37 @@ public class ApplicationService {
 
         Application savedApplication = applicationRepository.save(application);
 
-        ApplicationResponse akbankApplicationResponse = akbankServiceClient.createApplication(prepareAkbankApplicationRequest(user));
+        sendApplication(request, user);
+        sendNotification(savedApplication);
+
+        user.getApplicationList().add(savedApplication);
 
         return savedApplication;
     }
 
-    private AkbankApplicationRequest prepareAkbankApplicationRequest(User user) {
-        AkbankApplicationRequest applicationRequest = new AkbankApplicationRequest();
-
-        applicationRequest.setUserId(1L);
-
-        return applicationRequest;
+    private void sendApplication(ApplicationRequest request, User user) {
+        BankType bankType = BankType.valueOf(request.getBankName().toUpperCase());
+        BankServiceClient bankServiceClient = bankClientFactory.createBankClient(bankType);
+        BankApplicationRequest bankApplicationRequest = new BankApplicationRequest();
+        bankApplicationRequest.setUserId(user.getId());
+        bankServiceClient.createApplication(bankApplicationRequest);
     }
+
+    private void sendNotification(Application application) {
+        NotificationDTO notificationDTO = getNotification(application);
+        notificationProducer.sendNotification(notificationDTO);
+    }
+
+    private NotificationDTO getNotification(Application application) {
+        return NotificationDTO.builder()
+                .notificationType(NotificationType.EMAIL)
+                .message("Kredi başvurunuz alınmıştır. Başvuru durumunuzu takip edebilirsiniz.")
+                .email(application.getUser().getEmail())
+                .build();
+    }
+
+    public List<Application> getAll(String email) {
+        return applicationRepository.getAll(email);
+    }
+
 }
