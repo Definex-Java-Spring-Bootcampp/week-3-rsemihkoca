770de96d11921b3ecb7e8d610e48ffffd1babfc1
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-31 18:31:43 +0300

    part3 okay but mongodb:logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/listener/KafkaConsumerListener.java

--- logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/listener/KafkaConsumerListener.java
+++ logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/listener/KafkaConsumerListener.java
@@ -1,2 +1,35 @@
-package com.rsemihkoca.logconsumerservicemain.listener;public class KafkaConsumerListener {
+package com.rsemihkoca.logconsumerservicemain.listener;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rsemihkoca.logconsumerservicemain.repository.TransactionRepository;
+import com.rsemihkoca.logconsumerservicemain.document.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumerListener {
+
+    @Autowired
+    private final TransactionRepository transactionRepository;
+
+    public KafkaConsumerListener(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.group-id}")
+    public void listen(String message) {
+        System.out.printf("Received Messasge: [%s] %n", message);
+        try {
+            Transaction transaction = new ObjectMapper().readValue(message, Transaction.class);
+            Transaction save = transactionRepository.save(transaction);
+            System.out.println("Transaction saved to MongoDB" + save);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
