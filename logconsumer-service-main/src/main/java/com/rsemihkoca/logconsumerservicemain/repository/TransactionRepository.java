d4639a117dcf91f35b76917ce721be9f1e286bb7
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-31 18:31:43 +0300

    part3 okay but mongodb:logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/repository/TransactionRepository.java

--- logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/repository/TransactionRepository.java
+++ logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/repository/TransactionRepository.java
@@ -1,2 +1,10 @@
-package com.rsemihkoca.logconsumerservicemain.repository;public class TransactionRepository {
+package com.rsemihkoca.logconsumerservicemain.repository;
+
+import com.rsemihkoca.logconsumerservicemain.document.Transaction;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface TransactionRepository extends MongoRepository<Transaction, String> {
+
+
 }
