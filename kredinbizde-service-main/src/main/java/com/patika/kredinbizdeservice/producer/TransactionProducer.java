965784bf9c46159c0ce5b836b3848e32d7e0d64e
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-31 18:31:43 +0300

    part3 okay but mongodb:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/producer/TransactionProducer.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/producer/TransactionProducer.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/producer/TransactionProducer.java
@@ -3,25 +3,31 @@ package com.patika.kredinbizdeservice.producer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import com.patika.kredinbizdeservice.producer.dto.Transaction;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
-public class TransactionProducerService {
+@Scope("singleton")
+public class TransactionProducer {
 
-    @Value(value = "${spring.kafka.template.default-topic}")
-    private String defaultTopic;
+    @Value(value = "${spring.kafka.producer.topic}")
+    private String topicName;
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    public TransactionProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @SneakyThrows
     public void sendTransaction(final Transaction message) {
-        kafkaTemplate.send(defaultTopic, new ObjectMapper().writeValueAsString(message));
+        kafkaTemplate.send(topicName, new ObjectMapper().writeValueAsString(message));
         log.info("Transaction published to Kafka");
     }
 }
