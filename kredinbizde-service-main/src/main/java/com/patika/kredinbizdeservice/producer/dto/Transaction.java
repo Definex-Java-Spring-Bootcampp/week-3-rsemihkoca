246d34e4075e9af3b132cf02da5f3b2eb850f0ed
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-31 18:31:43 +0300

    part3 okay but mongodb:kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/producer/dto/Transaction.java

--- kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/producer/dto/Transaction.java
+++ kredinbizde-service-main/src/main/java/com/patika/kredinbizdeservice/producer/dto/Transaction.java
@@ -1,2 +1,18 @@
-package com.patika.kredinbizdeservice.producer.dto;public class Transaction {
+package com.patika.kredinbizdeservice.producer.dto;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@EqualsAndHashCode
+@ToString
+public class Transaction {
+    private String errorMessage;
+    private String sender;
+    private String statusCode;
+    private HttpStatus httpStatus;
+    private long timestamp;
 }
