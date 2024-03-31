539e88924d8af3b870b4e0df6a1acc8ca8317b0b
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-31 18:31:43 +0300

    part3 okay but mongodb:logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/document/Transaction.java

--- logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/document/Transaction.java
+++ logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/document/Transaction.java
@@ -1,2 +1,23 @@
-package com.rsemihkoca.logconsumerservicemain.document;public class Transaction {
+package com.rsemihkoca.logconsumerservicemain.document;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.http.HttpStatus;
+
+@Document(collection = "transactions")
+public class Transaction {
+    private String errorMessage;
+    private String sender;
+    private String statusCode;
+    private HttpStatus httpStatus;
+    private long timestamp;
+
+
+    public Transaction(String errorMessage, String sender, String statusCode, HttpStatus httpStatus, long timestamp) {
+        super();
+        this.errorMessage = errorMessage;
+        this.sender = sender;
+        this.statusCode = statusCode;
+        this.httpStatus = httpStatus;
+        this.timestamp = timestamp;
+    }
 }
