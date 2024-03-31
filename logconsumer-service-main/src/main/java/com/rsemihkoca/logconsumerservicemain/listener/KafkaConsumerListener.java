995d8c13e3e6b9ba59477d6129927e79fa99a85e
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 01:34:05 +0300

    part3 finished:logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/listener/KafkaConsumerListener.java

--- logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/listener/KafkaConsumerListener.java
+++ logconsumer-service-main/src/main/java/com/rsemihkoca/logconsumerservicemain/listener/KafkaConsumerListener.java
@@ -13,18 +13,22 @@ import org.springframework.stereotype.Component;
 public class KafkaConsumerListener {
 
     @Autowired
+    private final ObjectMapper objectMapper;
+
+    @Autowired
     private final TransactionRepository transactionRepository;
 
-    public KafkaConsumerListener(TransactionRepository transactionRepository) {
+    public KafkaConsumerListener(ObjectMapper objectMapper, TransactionRepository transactionRepository) {
+        this.objectMapper = objectMapper;
         this.transactionRepository = transactionRepository;
     }
 
-
     @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.group-id}")
+//    @KafkaListener(topics = "${spring.kafka.topic.name}")
     public void listen(String message) {
         System.out.printf("Received Messasge: [%s] %n", message);
         try {
-            Transaction transaction = new ObjectMapper().readValue(message, Transaction.class);
+            Transaction transaction = objectMapper.readValue(message, Transaction.class);
             Transaction save = transactionRepository.save(transaction);
             System.out.println("Transaction saved to MongoDB" + save);
         } catch (JsonProcessingException e) {
