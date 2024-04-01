b545bc6bcb02cc292237f13d13b3fa9850f5711b
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 18:54:15 +0300

    product service finished:onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/controller/OrderController.java

--- onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/controller/OrderController.java
+++ onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/controller/OrderController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("api/onlinealisveris/v1/orders")
+@RequestMapping("api/onlinealisveris/v1/order")
 @RequiredArgsConstructor
 public class OrderController {
 
