37bdf1fd7215bd22fe04f26b5ea63736d2499c96
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 18:54:15 +0300

    product service finished:onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/model/type/NonNegativeIntegerType.java

--- onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/model/type/NonNegativeIntegerType.java
+++ onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/model/type/NonNegativeIntegerType.java
@@ -1,2 +1,44 @@
-package com.rsemihkoca.productservicemain.model.type;public class NonNegativeIntegerType {
+package com.rsemihkoca.productservicemain.model.type;
+
+public class NonNegativeIntegerType {
+    private int value;
+
+    public NonNegativeIntegerType(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Value must be non-negative.");
+        }
+        this.value = value;
+    }
+
+    public void add (int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Value must be non-negative.");
+        }
+        if (Integer.MAX_VALUE - this.value < value) {
+            throw new IllegalArgumentException("Value must be less than or equal to " + (Integer.MAX_VALUE - this.value) + ".");
+        }
+        this.value += value;
+    }
+
+    public void subtract (int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Value must be non-negative.");
+        }
+        if (this.value < value) {
+            throw new IllegalArgumentException("Value must be greater than the value to be subtracted.");
+        }
+        this.value -= value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Value must be non-negative.");
+        }
+        this.value = value;
+    }
+
 }
