040114338c3265fb8d21cc595fd5c39f232a3cbd
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 15:27:27 +0300

    order and user service created now product service init:onlinealisveris/OnlineAlisveris/user-service-main/src/main/java/com/rsemihkoca/userservicemain/repository/UserRepository.java

--- onlinealisveris/OnlineAlisveris/user-service-main/src/main/java/com/rsemihkoca/userservicemain/repository/UserRepository.java
+++ onlinealisveris/OnlineAlisveris/user-service-main/src/main/java/com/rsemihkoca/userservicemain/repository/UserRepository.java
@@ -1,4 +1,22 @@
 package com.rsemihkoca.userservicemain.repository;
 
+import com.rsemihkoca.userservicemain.model.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class UserRepository {
+
+    private final List<User> userList = new ArrayList<>();
+
+    public User save(User user) {
+        userList.add(user);
+        return user;
+    }
+
+    public List<User> getAll() {
+        return userList;
+    }
 }
