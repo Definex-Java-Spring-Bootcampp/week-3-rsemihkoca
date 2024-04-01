f49177a13954461306444d23e95603c09a61dc84
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 15:27:27 +0300

    order and user service created now product service init:onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/model/Invoice.java

--- onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/model/Invoice.java
+++ onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/model/Invoice.java
@@ -1,2 +1,22 @@
-package com.rsemihkoca.orderservicemain.model;public class Invoice {
+package com.rsemihkoca.orderservicemain.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode
+public class Invoice {
+
+    private String id;
+    private double total = 0;
+
+    public void add(double amount) {
+        total += amount;
+    }
+
+    private Invoice(String id) {
+        this.id = id;
+    }
+
 }
