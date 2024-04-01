6667512aaa3074a35426a6be3ffbf9dbedb5c84f
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 18:54:15 +0300

    product service finished:onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/dto/StockTransaction.java

--- onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/dto/StockTransaction.java
+++ onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/dto/StockTransaction.java
@@ -1,2 +1,15 @@
-package com.rsemihkoca.productservicemain.dto;public class StockTransaction {
+package com.rsemihkoca.productservicemain.dto;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.http.HttpStatus;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class StockTransaction {
+    private String productId;
+    private Integer quantity;
 }
