68747e44e2eca50b3eb56e54ab5a923e7ae9b459
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 15:27:27 +0300

    order and user service created now product service init:onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/service/OrderService.java

--- onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/service/OrderService.java
+++ onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/service/OrderService.java
@@ -1,4 +1,31 @@
 package com.rsemihkoca.orderservicemain.service;
 
+import com.rsemihkoca.orderservicemain.model.Order;
+import com.rsemihkoca.orderservicemain.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+
+    public List<Order> getAll() {
+        return orderRepository.getAll();
+    }
 }
