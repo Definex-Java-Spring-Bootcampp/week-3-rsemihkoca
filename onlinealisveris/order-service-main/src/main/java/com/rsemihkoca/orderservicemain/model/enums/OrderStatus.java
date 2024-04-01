9ba3e7d8765cb7501af21c8caa45cbfeb6a5c22a
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 15:27:27 +0300

    order and user service created now product service init:onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/model/enums/OrderStatus.java

--- onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/model/enums/OrderStatus.java
+++ onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/model/enums/OrderStatus.java
@@ -1,2 +1,7 @@
-package com.rsemihkoca.orderservicemain.model.enums;public enum OrderStatus {
+package com.rsemihkoca.orderservicemain.model.enums;
+
+public enum OrderStatus {
+    PRE_ORDER,
+    ORDERED,
+    CANCEL
 }
