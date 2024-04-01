6352c2018438106594dc12021a89bc5238837d88
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 15:27:27 +0300

    order and user service created now product service init:onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/controller/OrderController.java

--- onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/controller/OrderController.java
+++ onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/controller/OrderController.java
@@ -1,12 +1,13 @@
 package com.rsemihkoca.orderservicemain.controller;
 
 
+import com.rsemihkoca.orderservicemain.model.Order;
+import com.rsemihkoca.orderservicemain.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -16,10 +17,54 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping
-    public Order createorder(@RequestBody Order request) {
-        return orderService.createorder(request);
+/*
+*
+    müşteriSepetGetir +
+    * /sepeteEkle: eğer açık sipariş yoksa yarat varsa +
+ona ekle iptal edene kadar,
+*
+/sipariş Onayla
+* /siparişIptal: sepetteki ürünleri siler
+    * */
+
+    @PostMapping("user/{userId}/approve")
+    public ResponseEntity<Order> approveOrder(@PathVariable Long userId) {
+        // userid user var mı kontrol et
+        // user varsa preorder var mı kontrol et
+        // pre order varsa statusu approve yap Siparişiniz alınmıştır.
+        // yoksa Sepetinizde ürün bulunmamaktadır dön
+        // Approvedan sonra rabitmq'ya fatura servise gönder
+        return ResponseEntity.ok(orderService.approveOrder());
     }
+
+    @PostMapping("user/{userId}/add/{productId}")
+    public ResponseEntity<Order> addProductToOrder(@PathVariable Long userId, @PathVariable String productId) {
+        // userid user var mı kontrol et
+        // user varsa order var mı şu an kontrol et
+        // order yoksa yeni order oluştur
+        // ürün stokta var mı kontrol et aynı üründen varsa toplu şekilde kontrol et
+        // stokta yoksa Sepetinizde ürün bulunmamaktadır dön veya stokta 2 ürün var 3 ürün eklemeye çalışıyorsa Sepetinizde ürün bulunmamaktadır dön
+        // içine ürünü ekle invoice güncelle.
+        return ResponseEntity.ok(orderService.addProductToOrder());
+    }
+
+    @PostMapping("user/{userId}/cancel")
+    public ResponseEntity<Order> cancelOrder(@PathVariable Long userId) {
+        // userid user var mı kontrol et
+        // user varsa order var mı kontrol et
+        // preorder varsa statusu cancel yap Cancelled Order dön
+        // yoksa Sepetinizde ürün bulunmamaktadır dön
+        return ResponseEntity.ok(orderService.cancelOrder());
+    }
+
+    @GetMapping("user/{userId}")
+    public ResponseEntity<Order> getOrderofCustomer(@PathVariable Long userId) {
+        // userid user var mı kontrol et
+        // user varsa order var mı kontrol et
+        // pre order varsa preorder dön yoksa Sepetinizde ürün bulunmamaktadır dön
+        return ResponseEntity.ok(orderService.getOrderofCustomer());
+    }
+
 
     @GetMapping
     public ResponseEntity<List<Order>> getAll() {
