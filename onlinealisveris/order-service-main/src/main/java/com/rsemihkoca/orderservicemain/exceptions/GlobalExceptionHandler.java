331be25e1d9b5b4f0b85c0eaade525ff1e24dc9f
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 15:27:27 +0300

    order and user service created now product service init:onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/exceptions/GlobalExceptionHandler.java

--- onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/exceptions/GlobalExceptionHandler.java
+++ onlinealisveris/order-service-main/src/main/java/com/rsemihkoca/orderservicemain/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.rsemihkoca.orderservicemain.exceptions;
 
-import com.rsemihkoca.userservicemain.exceptions.dto.ExceptionResponse;
-import com.rsemihkoca.userservicemain.producer.TransactionProducer;
-import com.rsemihkoca.userservicemain.producer.dto.Transaction;
+import com.rsemihkoca.orderservicemain.exceptions.dto.ExceptionResponse;
+import com.rsemihkoca.orderservicemain.producer.TransactionProducer;
+import com.rsemihkoca.orderservicemain.producer.dto.Transaction;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,7 +39,7 @@ public class GlobalExceptionHandler {
     private Transaction prepareTransaction(Exception exception) {
         return Transaction.builder()
                 .errorMessage(exception.getMessage())
-                .sender("user-service-main")
+                .sender("order-service-main")
                 .httpStatus(HttpStatus.BAD_REQUEST)
                 .statusCode(String.valueOf(HttpStatus.BAD_REQUEST.value()))
                 .timestamp(System.currentTimeMillis())
