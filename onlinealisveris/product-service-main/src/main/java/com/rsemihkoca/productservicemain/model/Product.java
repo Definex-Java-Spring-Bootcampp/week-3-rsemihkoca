82f4e002f1070f04a0117860bb5058e4dbb5d629
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 18:54:15 +0300

    product service finished:onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/model/Product.java

--- onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/model/Product.java
+++ onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/model/Product.java
@@ -1,10 +1,21 @@
 package com.rsemihkoca.productservicemain.model;
 
+import com.rsemihkoca.productservicemain.model.enums.Category;
+import com.rsemihkoca.productservicemain.model.type.NonNegativeIntegerType;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class Product {
+    private String productId;
     private String name;
     private double price;
-    private int stock;
+    private NonNegativeIntegerType stock;
     private Category category;
+
 }
 
 
