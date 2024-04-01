da8c4cab47b06e0aee7b1fb58b45df6bd478b879
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 15:27:27 +0300

    order and user service created now product service init:onlinealisveris/OnlineAlisveris/user-service-main/src/main/java/com/rsemihkoca/userservicemain/model/User.java

--- onlinealisveris/OnlineAlisveris/user-service-main/src/main/java/com/rsemihkoca/userservicemain/model/User.java
+++ onlinealisveris/OnlineAlisveris/user-service-main/src/main/java/com/rsemihkoca/userservicemain/model/User.java
@@ -1,2 +1,20 @@
-package com.rsemihkoca.userservicemain.model;public class User {
+package com.rsemihkoca.userservicemain.model;
+
+
+import lombok.*;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class User {
+
+    private String userId;
+    private String name;
+    private Integer age;
 }
