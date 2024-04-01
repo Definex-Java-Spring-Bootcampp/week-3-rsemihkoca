6f49b55f899df37bdac9af0c9c8ea7272c56cf07
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 18:54:15 +0300

    product service finished:onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/ProductServiceMainApplication.java

--- onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/ProductServiceMainApplication.java
+++ onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/ProductServiceMainApplication.java
@@ -1,15 +1,26 @@
 package com.rsemihkoca.productservicemain;
 
+import com.rsemihkoca.productservicemain.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductServiceMainApplication {
+@RequiredArgsConstructor
+public class ProductServiceMainApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceMainApplication.class, args);
     }
 
+    private final ProductRepository productRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        productRepository.initializeDatabase();
+        System.out.println("Data Initialized...");
+    }
 }
