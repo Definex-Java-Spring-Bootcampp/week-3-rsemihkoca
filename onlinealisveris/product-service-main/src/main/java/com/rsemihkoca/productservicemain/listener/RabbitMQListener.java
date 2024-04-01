79c52a4d8a396c450be493fdc26e860b45e9765a
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 18:54:15 +0300

    product service finished:onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/listener/RabbitMQListener.java

--- onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/listener/RabbitMQListener.java
+++ onlinealisveris/product-service-main/src/main/java/com/rsemihkoca/productservicemain/listener/RabbitMQListener.java
@@ -1,50 +1,50 @@
 package com.rsemihkoca.productservicemain.listener;
 
-import com.patika.notificationservice.dto.NotificationDTO;
-import com.patika.notificationservice.dto.enums.NotificationType;
-import com.patika.notificationservice.service.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rsemihkoca.productservicemain.dto.StockTransaction;
+import com.rsemihkoca.productservicemain.model.Product;
+import com.rsemihkoca.productservicemain.model.type.NonNegativeIntegerType;
+import com.rsemihkoca.productservicemain.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class NotificationListener {
+@RequiredArgsConstructor
+public class RabbitMQListener {
 
-    private final NotificationService notificationService;
+    private final ObjectMapper objectMapper;
 
-    private final SmsNotificationStrategy smsNotificationStrategy;
-    private final EmailNotificationStrategy emailNotificationStrategy;
-    private final MobileNotificationStrategy mobileNotificationStrategy;
+    private final ProductService productService;
 
-    public NotificationListener(NotificationService notificationService,
-                                SmsNotificationStrategy smsNotificationStrategy,
-                                EmailNotificationStrategy emailNotificationStrategy,
-                                MobileNotificationStrategy mobileNotificationStrategy) {
-        this.notificationService = notificationService;
-        this.smsNotificationStrategy = smsNotificationStrategy;
-        this.emailNotificationStrategy = emailNotificationStrategy;
-        this.mobileNotificationStrategy = mobileNotificationStrategy;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue.increase}")
+    public void increaseStock(String message) {
+        StockTransaction stockTransaction;
+        try {
+            stockTransaction = objectMapper.readValue(message, StockTransaction.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        Product product = productService.getProductById(stockTransaction.getProductId());
+        int newStock = product.getStock().getValue() + stockTransaction.getQuantity();
+        product.setStock(new NonNegativeIntegerType(newStock));
     }
 
-    @RabbitListener(queues = "${rabbitmq.queue}")
-    public void sendNotification(NotificationDTO notificationDTO) {
-        log.info("Notification received: {}", notificationDTO);
-        NotificationStrategy notificationStrategy = getNotificationType(notificationDTO);
-        String message = notificationDTO.getMessage();
-        notificationService.setNotificationStrategy(notificationStrategy);
-        notificationService.sendNotification(message);
-
+    @RabbitListener(queues = "${spring.rabbitmq.queue.decrease}")
+    public void decreaseStock(String message) {
+        StockTransaction stockTransaction;
+        try {
+            stockTransaction = objectMapper.readValue(message, StockTransaction.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        Product product = productService.getProductById(stockTransaction.getProductId());
+        int newStock = product.getStock().getValue() - stockTransaction.getQuantity();
+        product.setStock(new NonNegativeIntegerType(newStock));
     }
-
-    private NotificationStrategy getNotificationType(NotificationDTO notificationDTO) {
-        NotificationType notificationType = notificationDTO.getNotificationType();
-        return switch (notificationType) {
-            case SMS -> smsNotificationStrategy;
-            case EMAIL -> emailNotificationStrategy;
-            case MOBILE_NOTIFICATION -> mobileNotificationStrategy;
-        };
-    }
-
 
 }
