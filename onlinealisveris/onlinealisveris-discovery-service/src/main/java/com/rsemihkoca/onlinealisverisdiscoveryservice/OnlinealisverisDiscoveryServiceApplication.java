56a187f8fabfb48ce4e5cc15c9244c01904959d5
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 15:27:27 +0300

    order and user service created now product service init:onlinealisveris/onlinealisveris-discovery-service/src/main/java/com/rsemihkoca/onlinealisverisdiscoveryservice/OnlinealisverisDiscoveryServiceApplication.java

--- onlinealisveris/onlinealisveris-discovery-service/src/main/java/com/rsemihkoca/onlinealisverisdiscoveryservice/OnlinealisverisDiscoveryServiceApplication.java
+++ onlinealisveris/onlinealisveris-discovery-service/src/main/java/com/rsemihkoca/onlinealisverisdiscoveryservice/OnlinealisverisDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.rsemihkoca.onlinealisverisdiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class OnlinealisverisDiscoveryServiceApplication {
 
     public static void main(String[] args) {
