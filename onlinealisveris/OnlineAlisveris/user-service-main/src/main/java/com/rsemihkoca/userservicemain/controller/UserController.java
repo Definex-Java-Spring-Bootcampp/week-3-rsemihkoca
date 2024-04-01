fdfafc9c5ed11df8213ed7e1a5817c14b96ace2c
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-01 15:27:27 +0300

    order and user service created now product service init:onlinealisveris/OnlineAlisveris/user-service-main/src/main/java/com/rsemihkoca/userservicemain/controller/UserController.java

--- onlinealisveris/OnlineAlisveris/user-service-main/src/main/java/com/rsemihkoca/userservicemain/controller/UserController.java
+++ onlinealisveris/OnlineAlisveris/user-service-main/src/main/java/com/rsemihkoca/userservicemain/controller/UserController.java
@@ -1,2 +1,34 @@
-package com.rsemihkoca.userservicemain.controller;public class UserController {
+package com.rsemihkoca.userservicemain.controller;
+
+
+import com.rsemihkoca.userservicemain.model.User;
+import com.rsemihkoca.userservicemain.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/onlinealisveris/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User request) {
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        return ResponseEntity.ok(userService.getAll());
+    }
+
+    @GetMapping("errorTest")
+    public ResponseEntity<List<User>> testKafka() {
+        throw new RuntimeException("test exception");
+    }
+
 }
